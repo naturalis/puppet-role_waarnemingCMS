86cb87844f6defd4ce25b69f06463410838d912c
Edwin Hermans <edwin@madtech.cx>
2017-07-06 21:31:53 +0100

    typo

--- manifests/init.pp
+++ manifests/init.pp
@@ -75,7 +75,7 @@ class role_waarnemingcms (
     server_name          => ['iobs.observation.org', 'support.observation.org', 'cms.example.com'],
     use_default_location => false,
     www_root             => $web_root,
-    server_cfg_prepent   => {
+    server_cfg_prepend   => {
       server_name_in_redirect => 'off',
     },
     locations            => {
